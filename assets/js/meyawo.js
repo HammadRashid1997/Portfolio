e3f2cc08a803ee358337b05ed2e9c694f0013189
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2024-04-14 20:15:21 +0500

    Create meyawo.js

---
+++ assets/js/meyawo.js
@@ -0,0 +1,69 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+$(document).ready(function() {
+    // Hide rows 3 and onwards initially
+    $('#portfolio .row:gt(2)').hide();
+
+    $('#view-more-btn').click(function() {
+        // Toggle visibility of rows 3 and onwards
+        $('#portfolio .row:gt(2)').toggle();
+        $('#portfolio .row').show();
+        // Hide the button
+        $(this).hide();
+    });
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+}
